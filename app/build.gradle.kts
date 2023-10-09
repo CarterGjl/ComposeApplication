28642d357159794e1d1f08b20acc68db28638beb
gaojinliang <gaojinliang@baidu.com>
2023-10-09 15:24:02 +0800

    使用 kotlin kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,102 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.composeapplicationabc"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+    namespace = "com.example.composeapplication"
+}
+
+dependencies {
+    val composeVersion = "1.6.0-alpha07"
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+    implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
+    implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.8")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    val livecycleVersion = "2.6.2"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$livecycleVersion")
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$livecycleVersion")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$livecycleVersion")
+
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+    val accompanistVersion = "0.33.2-alpha"
+    implementation("com.google.accompanist:accompanist-webview:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-permissions:$accompanistVersion")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    // 替代sharepreference
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences-core:1.0.0")
+    implementation("androidx.paging:paging-compose:3.2.1")
+    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+
+    val cameraxVersion = "1.2.3"
+    // CameraX core library using camera2 implementation
+    implementation("androidx.camera:camera-camera2:$cameraxVersion")
+    // CameraX Lifecycle Library
+    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
+    // CameraX View class
+    implementation("androidx.camera:camera-view:$cameraxVersion")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    val version = "2.7.4"
+    implementation("androidx.navigation:navigation-compose:$version")
+    implementation("androidx.navigation:navigation-fragment-ktx:$version")
+    implementation("androidx.navigation:navigation-ui-ktx:$version")
+    implementation("com.airbnb.android:lottie-compose:6.1.0")
+
+    val media3version = "1.1.1"
+    implementation("androidx.media3:media3-exoplayer:$media3version")
+    implementation("androidx.media3:media3-ui:$media3version")
+    implementation("androidx.media3:media3-session:$media3version")
+}
