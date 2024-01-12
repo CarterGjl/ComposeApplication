a2abb2d596d398dd6ed7aae6e62c62421d3a15c1
gaojinliang <gaojinliang@baidu.com>
2024-01-12 15:19:08 +0800

    升级api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,10 +49,10 @@ android {
 }
 
 dependencies {
-    val composeVersion = "1.6.0-beta01"
+    val composeVersion = "1.6.0-rc01"
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
@@ -66,7 +66,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     val livecycleVersion = "2.6.2"
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$livecycleVersion")
@@ -89,7 +89,7 @@ dependencies {
     implementation("androidx.paging:paging-compose:3.2.1")
     implementation("androidx.paging:paging-runtime-ktx:3.2.1")
 
-    val cameraxVersion = "1.3.0"
+    val cameraxVersion = "1.3.1"
     // CameraX core library using camera2 implementation
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     // CameraX Lifecycle Library
@@ -97,11 +97,11 @@ dependencies {
     // CameraX View class
     implementation("androidx.camera:camera-view:$cameraxVersion")
     implementation("androidx.compose.material3:material3:1.1.2")
-    val version = "2.7.5"
+    val version = "2.7.6"
     implementation("androidx.navigation:navigation-compose:$version")
     implementation("androidx.navigation:navigation-fragment-ktx:$version")
     implementation("androidx.navigation:navigation-ui-ktx:$version")
-    implementation("com.airbnb.android:lottie-compose:6.2.0")
+    implementation("com.airbnb.android:lottie-compose:6.3.0")
 
     val media3version = "1.2.0"
     implementation("androidx.media3:media3-exoplayer:$media3version")
--- app/src/main/java/com/example/composeapplication/activity/bsae/BaseActivity.kt
+++ app/src/main/java/com/example/composeapplication/activity/bsae/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.example.composeapplication.activity.bsae
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 
